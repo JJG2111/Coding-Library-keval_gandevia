7dec941ad40a83f8e647983fb28d99c2dc664992
jenilgandhi2111 <63243203+jenilgandhi2111@users.noreply.github.com>
2020-09-30 20:17:05 +0530

    Update prim_mst.c
    This is updated prims.c

--- prim_mst.c
+++ prim_mst.c
@@ -1,99 +1,102 @@
-/*
-CREATED by Gandevia keval
-MOTTO:
-Work before burgers!!!
-Date:30/09/2020
-*/
-#include <stdio.h>
-#include <string.h>
-#include <math.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#define INT_MAX 10000000
-
-
-int min_key(int key[],bool set[],int n) {
-    int min = INT_MAX,index_min,i;
-    for(i=0;i<n;i++) {
-        if(set[i] != true && key[i] < min) {
-            min = key[i];
-            index_min = i;
-        }
-    }
-    return index_min;
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+#include<limits.h>
+void c_p_c()
+{
+    
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
 }
+void solve_prims(int src_node,int n,int arr[n][n])
+{
 
-void print(int parent[],int n,int graph[][n],int source) {
-    int i;
-    for(i=0;i<n;i++) {
-        if(i != source - 1)
-            printf("%d-%d\t%d\n",i+1,parent[i]+1,graph[i][parent[i]]);
-    }
+
+	bool visited[n];
+	for(int i=0;i<n;i++)visited[i]=false;
+	visited[src_node]=true;
+	for(int i=0;i<n-1;i++)
+	{
+		int parent;
+		int child;
+		int min=INT_MAX;
+		//this is the loop which will keep track of no of iters counter loop
+		for (int j=0;j<n;j++)
+		{
+			//this loop is for visited array cntr
+			if(visited[j]==true)
+			{
+				//now iterate over the adjacents and pick minimum
+				for(int k=0;k<n;k++)
+				{
+					if(arr[j][k]<=min && arr[j][k]!=0 && arr[j][k]!=-1 && visited[k]==false)
+					{
+						min=arr[j][k];
+						parent=j;
+						child=k;
+					}
+				}
+			}
+		}
+		// printf("parent %d chid %d weight %d\n",parent,child,min);
+		arr[parent][child]=-1;
+		arr[child][parent]=-1;
+		visited[child]=true;
+		visited[parent]=true;
+	}
+
+
 }
+int main()
+{
 
-void prim(int n,int graph[][n],int source) {
-    
-    int parent[n],key[n],i,j,index;
-    bool set[n];
-    for(i=0;i<n;i++) {
-        key[i] = INT_MAX;
-        set[i] = false;
-    }
-    
-    key[source - 1] = 0;
-    parent[source - 1] = -1;
-    
-    for(i=0;i<n;i++) {
-        index = min_key(key,set,n);
-        set[index] = true;
-        for(j=0;j<n;j++) {
-            if(graph[index][j] && (set[j] == false) && graph[index][j] < key[j]) {
-                parent[j] = index;
-                key[j] = graph[index][j];
-            }
-        }
-    }
-    
-    print(parent,n,graph,source);
+	c_p_c();
+	int n;
+	scanf("%d",&n);
+	int arr[n][n];
+	int clone[n][n];
+	for(int i=0;i<n;i++)
+	{
+		for(int j=0;j<n;j++)
+		{
+			scanf("%d",&arr[i][j]);
+			clone[i][j]=arr[i][j];
+			if(i>j)
+			{
+				if (arr[i][j]!=arr[j][i])
+				{
+					printf("Weight of the edge %d - %d must be same as edge %d - %d",i+1,j+1,j+1,i+1);
+					exit(0);
+				}
+			}
+			else if(i==j&& arr[i][j]!=0)
+			{
+				printf("Weight of the edge %d - %d must be 0",i+1,i+1);
+				exit(0);
+			}
+			else if(arr[i][j]<0)
+			{
+				printf("Weight of the edge %d - %d can not be negative",i+1,j+1);
+				exit(0);
+			}
+		}
+	}
+	int src_node;
+	scanf("%d",&src_node);
+	solve_prims(src_node-1,n,arr);
+	for(int i=0;i<n;i++)
+	{
+		bool flag=false;
+		for(int j=0;j<n;j++)
+		{
+			if(arr[i][j]==-1 && i+1!=src_node && flag==false){
+				arr[j][i]=0;
+				printf("%d-%d\t%d\n",i+1,j+1,clone[i][j]);
+				flag=true;	
+			}
+			
+		}
+	}
 }
-
-int main() {
-    int n,i,j,source;
-    scanf("%d",&n);
-    int graph[n][n];
-    
-    for(i=0;i<n;i++) {
-        for(j=0;j<n;j++) {
-            scanf("%d",&graph[i][j]);
-            
-            if(j<i) {
-                if(graph[j][i] != graph[i][j]) {
-                    printf("Weight of the edge %d - %d must be same as edge %d - %d\n",i+1,j+1,j+1,i+1);
-                    exit(0);
-                }
-            }
-            
-            if(j != i) {
-                if(graph[i][j] < 0) {
-                    printf("Weight of the edge %d - %d can not be negative\n",i+1,j+1);
-                    exit(0);
-                }
-            }
-            
-            if(i == j) {
-                if(graph[i][j] != 0) {
-                    printf("Weight of the edge %d - %d must be 0\n",i+1,j+1);
-                    exit(0);
-                }
-            }
-        }
-        
-        
-    }
-    
-    scanf("%d",&source);
-    prim(n,graph,source);
-    
-    return 0;
-}
-
